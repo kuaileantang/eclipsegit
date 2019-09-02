3003c42e918f3fe676fa9fc4a36599aea9ca195a
tom_sys <40227115@qq.com>
2019-09-02 21:59:05 +0800

    myfirst  commit

---
+++ src/demo.java
@@ -0,0 +1,26 @@
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.sin(Math.PI / 2));
+		System.out.println(Math.exp(2));
+		System.out.println(Math.log(2));
+		System.out.println(Math.log10(2));
+		System.out.println(Math.sqrt(4));
+		System.out.println(Math.cbrt(8));
+		System.out.println(Math.pow(2, 5));// 2的5次方
+		// 比较运算
+		System.out.println(Math.max(4, 8));
+		System.out.println(Math.min(4, 8));
+
+		// 绝对值
+		System.out.println(Math.abs(-7));
+
+		// 四舍五入
+		System.out.println(Math.floor(2.5));// 向下取整
+		System.out.println(Math.round(2.5));// 四舍五入
+
+	}
+
+}
